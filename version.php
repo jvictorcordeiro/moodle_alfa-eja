3b4cde217cecf2c8faa8b9b0b927e95ffda53b25
Raí Araujo <raisanhacu@gmail.com>
2025-08-06 09:34:33 -0300

    alterando versão

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 
 
 $plugin->component = 'theme_alfa';
-$plugin->version = 2025040200;
+$plugin->version = 2025080600;
 $plugin->requires = 2022041900;
 $plugin->dependencies = [
     'theme_boost' => ANY_VERSION
