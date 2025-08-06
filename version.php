67daf09f6ab65ef2c635e90bf872b81f6b6b5986
João Victor Cordeiro <50725830+jvictorcordeiro@users.noreply.github.com>
2025-08-06 14:08:44 -0300

    Update version.php

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 
 
 $plugin->component = 'theme_alfa';
-$plugin->version = 2025080600;
+$plugin->version = 202508061430;
 $plugin->requires = 2022041900;
 $plugin->dependencies = [
     'theme_boost' => ANY_VERSION
